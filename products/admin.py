8439490db6ceccafa0f791a91ae84f813342ff5b
jnematullayeva <jnematullayeva@example.com>
2026-04-10 14:48:24 +0500

    1 commit

--- products/admin.py
+++ products/admin.py
@@ -4,7 +4,6 @@ from .models import Category, Product
 admin.site.register(Category)
 
 @admin.register(Product)
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'price']
     list_filter = ['price']
